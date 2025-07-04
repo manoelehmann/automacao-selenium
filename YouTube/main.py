627b2fb354417fe6f12b781dad4cbe044dd8f8bc
Manoel Neto <manoelnetoteixeirarodrigues@gmail.com>
2025-07-04 11:42:34 -0300

    instalando a biblioteca do selenium

---
+++ YouTube/main.py
@@ -0,0 +1,5 @@
+from selenium import webdriver
+import time
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC

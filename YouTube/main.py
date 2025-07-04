c7f567c6c0eb8b3373fd2f996617ceb940a6cdf9
Manoel Neto <manoelnetoteixeirarodrigues@gmail.com>
2025-07-04 12:36:26 -0300

    planejamento

--- YouTube/main.py
+++ YouTube/main.py
@@ -3,3 +3,20 @@ import time
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
+navegador = webdriver.Chrome()
+navegador.get('https://www.youtube.com/')
+navegador.maximize_window()
+
+navegador.find_element('class name', 'ytSearchboxComponentInput.yt-searchbox-input.title').send_keys('Maxxsoft')
+navegador.find_element('class name', 'ytSearchboxComponentSearchButton').click()
+
+
+
+
+
+'''FAZER UMA AUTOMAÇÃO PARA PESQUISAR O VIDEO MAIS RECENTE DA MAXXSOFT USANDO FILTRO'''
+
+
+
+time.sleep(1000)

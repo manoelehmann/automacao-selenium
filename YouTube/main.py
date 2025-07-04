e402534a8bc6dfd5dac7985ebdb0ce8c27038100
Manoel Neto <manoelnetoteixeirarodrigues@gmail.com>
2025-07-04 13:54:25 -0300

    correções de alguns bugs

--- YouTube/main.py
+++ YouTube/main.py
@@ -7,12 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 navegador = webdriver.Chrome()
 navegador.get('https://www.youtube.com/')
 navegador.maximize_window()
+espera = WebDriverWait(navegador, 20)
 
 navegador.find_element('class name', 'ytSearchboxComponentInput.yt-searchbox-input.title').send_keys('Maxxsoft')
-navegador.find_element('class name', 'ytSearchboxComponentSearchButton').click()
+clicar_pesquisar = navegador.find_element('class name', 'ytSearchboxComponentSearchButton')
+clicar_pesquisar.click()
+time.sleep(1)
 
-
-
+navegador.find_element('class name', 'yt-spec-button-shape-next.yt-spec-button-shape-next--text.yt-spec-button-shape-next--mono').click()
 
 
 '''FAZER UMA AUTOMAÇÃO PARA PESQUISAR O VIDEO MAIS RECENTE DA MAXXSOFT USANDO FILTRO'''

026b0cd4677d6053659dcd529c6b8287cbfb39e9
Manoel Neto <manoelnetoteixeirarodrigues@gmail.com>
2025-07-07 16:08:30 -0300

    versão final

--- YouTube/main.py
+++ YouTube/main.py
@@ -9,18 +9,21 @@ navegador.get('https://www.youtube.com/')
 navegador.maximize_window()
 espera = WebDriverWait(navegador, 20)
 
+
 navegador.find_element('class name', 'ytSearchboxComponentInput.yt-searchbox-input.title').send_keys('Maxxsoft')
 clicar_pesquisar = navegador.find_element('class name', 'ytSearchboxComponentSearchButton')
 clicar_pesquisar.click()
 time.sleep(1)
 
 def filtro(navegador):
-    botao_filtro = navegador.find_elements('class name', 'style-scope.ytd-search-header-renderer')
+    botao_filtro = navegador.find_elements('id', 'filter-button')
     for botao in botao_filtro:
         if 'Filtros' in botao.text:
             botao.click()
             break
+
 filtro(navegador)
+
 
 selecionar_filtro = navegador.find_elements('id', 'label')
 for botao in selecionar_filtro:
@@ -28,20 +31,27 @@ for botao in selecionar_filtro:
         botao.click()
         break
 
-time.sleep(0.5)
+time.sleep(1)
 
 filtro(navegador)
 
+time.sleep(1)
 selecionar_data = navegador.find_elements('id', 'label')
 for botao in selecionar_data:
     if 'Data de envio' in botao.text:
         botao.click()
         break
 
-time.sleep(0.5)
-canal = navegador.find_elements('id', 'main-link')
+time.sleep(1)
+canal = navegador.find_elements('id', 'channel-title')
 for botao in canal:
     if 'MaxxSoft Tecnologia' in botao.text:
+        botao.click()
+        break
+time.sleep(0.5)
+video = navegador.find_elements('id', 'video-title')
+for botao in video:
+    if 'WEBINAR - Desvendando o Agronegócio com a MaxxSoft - EP 3' in botao.text:
         botao.click()
         break
 
